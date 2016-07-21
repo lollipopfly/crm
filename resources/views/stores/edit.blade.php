fee358c2dd03c75883266544e5da178dabe36cbb
lollipopfly <lollipop.fly@gmail.com>
2016-07-22 00:00:50 +0300

    finalize Stores

--- resources/views/stores/edit.blade.php
+++ resources/views/stores/edit.blade.php
@@ -1,67 +1,94 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container">
+    <div class="bg-light lter b-b wrapper-md">
+        <h1 class="m-b-sm font-thin h3">Edit store</h1>
+        <a href="{{ url('/stores/') }}" class="btn btn-default"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> Back</a>
+    </div>
 
-    <h1>Edit Store {{ $store->id }}</h1>
+    <div class="wrapper-md">
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                       {!! Form::model($store, [
+                           'method' => 'PATCH',
+                           'url' => ['/stores', $store->id],
+                           'class' => 'form-horizontal'
+                       ]) !!}
 
-    {!! Form::model($store, [
-        'method' => 'PATCH',
-        'url' => ['/stores', $store->id],
-        'class' => 'form-horizontal'
-    ]) !!}
+                           <div class="row">
+                               <div class="col-md-1"></div>
+                               <div class="col-md-3 m-t-xs">
+                                   <span class="text"><i class="fa fa-building-o text"></i> Name:</span>
+                               </div>
+                               <div class="col-md-7 {{ $errors->has('name') ? 'has-error' : '' }}">
+                                   {!! Form::text('name', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                                   {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
+                               </div>
+                           </div>
 
-                <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
-                {!! Form::label('name', 'Name', ['class' => 'col-sm-3 control-label']) !!}
-                <div class="col-sm-6">
-                    {!! Form::text('name', null, ['class' => 'form-control', 'required' => 'required']) !!}
-                    {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
+                           <div class="line line-dashed b-b line-lg pull-in"></div>
+                           <div class="row">
+                               <div class="col-md-1"></div>
+                               <div class="col-md-3 m-t-xs">
+                                   <span class="text"><i class="fa icon-user text"></i> Owner name:</span>
+                               </div>
+                               <div class="col-md-7 {{ $errors->has('owner_name') ? 'has-error' : '' }}">
+                                   {!! Form::text('owner_name', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                                   {!! $errors->first('owner_name', '<p class="help-block">:message</p>') !!}
+                               </div>
+                           </div>
+
+                           <div class="line line-dashed b-b line-lg pull-in"></div>
+                           <div class="row">
+                               <div class="col-md-1"></div>
+                               <div class="col-md-3 m-t-xs">
+                                   <span class="text"><i class="fa icon-map text"></i> Address:</span>
+                               </div>
+                               <div class="col-md-7 {{ $errors->has('address') ? 'has-error' : '' }}">
+                                   {!! Form::text('address', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                                   {!! $errors->first('address', '<p class="help-block">:message</p>') !!}
+                               </div>
+                           </div>
+
+                           <div class="line line-dashed b-b line-lg pull-in"></div>
+                           <div class="row">
+                               <div class="col-md-1"></div>
+                               <div class="col-md-3 m-t-xs">
+                                   <span class="text"><i class="fa icon-screen-smartphone text"></i> Phone:</span>
+                               </div>
+                               <div class="col-md-7 {{ $errors->has('phone') ? 'has-error' : '' }}">
+                                   {!! Form::text('phone', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                                   {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
+                               </div>
+                           </div>
+
+                           <div class="line line-dashed b-b line-lg pull-in"></div>
+                           <div class="row">
+                               <div class="col-md-1"></div>
+                               <div class="col-md-3 m-t-xs">
+                                   <span class="text"><i class="fa icon-envelope-open text"></i> Email:</span>
+                               </div>
+                               <div class="col-md-7 {{ $errors->has('email') ? 'has-error' : '' }}">
+                                   {!! Form::text('email', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                                   {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
+                               </div>
+                           </div>
+
+                           <div class="line line-dashed b-b line-lg pull-in"></div>
+                           <div class="row">
+                           <div class="col-md-4"></div>
+                               <div class="col-xs-7 text-right">
+                                   {!! Form::submit('Update', ['class' => 'btn btn-primary text-u-c']) !!}
+                               </div>
+                           </div>
+                       {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
-            <div class="form-group {{ $errors->has('address') ? 'has-error' : ''}}">
-                {!! Form::label('address', 'Address', ['class' => 'col-sm-3 control-label']) !!}
-                <div class="col-sm-6">
-                    {!! Form::text('address', null, ['class' => 'form-control', 'required' => 'required']) !!}
-                    {!! $errors->first('address', '<p class="help-block">:message</p>') !!}
-                </div>
-            </div>
-            <div class="form-group {{ $errors->has('phone') ? 'has-error' : ''}}">
-                {!! Form::label('phone', 'Phone', ['class' => 'col-sm-3 control-label']) !!}
-                <div class="col-sm-6">
-                    {!! Form::text('phone', null, ['class' => 'form-control', 'required' => 'required']) !!}
-                    {!! $errors->first('phone', '<p class="help-block">:message</p>') !!}
-                </div>
-            </div>
-            <div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
-                {!! Form::label('email', 'Email', ['class' => 'col-sm-3 control-label']) !!}
-                <div class="col-sm-6">
-                    {!! Form::text('email', null, ['class' => 'form-control']) !!}
-                    {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
-                </div>
-            </div>
-            <div class="form-group {{ $errors->has('owner_name') ? 'has-error' : ''}}">
-                {!! Form::label('owner_name', 'Owner Name', ['class' => 'col-sm-3 control-label']) !!}
-                <div class="col-sm-6">
-                    {!! Form::text('owner_name', null, ['class' => 'form-control']) !!}
-                    {!! $errors->first('owner_name', '<p class="help-block">:message</p>') !!}
-                </div>
-            </div>
-
-
-    <div class="form-group">
-        <div class="col-sm-offset-3 col-sm-3">
-            {!! Form::submit('Update', ['class' => 'btn btn-primary form-control']) !!}
         </div>
     </div>
-    {!! Form::close() !!}
 
-    @if ($errors->any())
-        <ul class="alert alert-danger">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-
-</div>
 @endsection
--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -1,33 +1,106 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container">
-
-    <h1>Store {{ $store->id }}
-        <a href="{{ url('stores/' . $store->id . '/edit') }}" class="btn btn-primary btn-xs" title="Edit Store"><span class="glyphicon glyphicon-pencil" aria-hidden="true"/></a>
+    <div class="bg-light lter b-b wrapper-md">
+        <h1 class="m-b-sm font-thin h3">{{ $store->name }}</h1>
+        <a href="{{ url('/stores/') }}" class="btn btn-default"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> Back</a>
+        <a href="{{ url('stores/' . $store->id . '/edit') }}" class="btn btn-primary">Edit</a>
         {!! Form::open([
             'method'=>'DELETE',
             'url' => ['stores', $store->id],
             'style' => 'display:inline'
         ]) !!}
-            {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true"/>', array(
+            {!! Form::button('Delete', array(
                     'type' => 'submit',
-                    'class' => 'btn btn-danger btn-xs',
+                    'class' => 'btn btn-danger',
                     'title' => 'Delete Store',
                     'onclick'=>'return confirm("Confirm delete?")'
             ));!!}
         {!! Form::close() !!}
-    </h1>
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped table-hover">
-            <tbody>
-                <tr>
-                    <th>ID</th><td>{{ $store->id }}</td>
-                </tr>
-                <tr><th> Name </th><td> {{ $store->name }} </td></tr><tr><th> Address </th><td> {{ $store->address }} </td></tr><tr><th> Phone </th><td> {{ $store->phone }} </td></tr>
-            </tbody>
-        </table>
     </div>
 
-</div>
+    <div class="wrapper-md">
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-3">
+                                <span class="text"><i class="fa fa-building-o text"></i> Name:</span>
+                            </div>
+                            <div class="col-md-7">
+                                <span class="text">
+                                    @if ($store->name)
+                                        {{ $store->name }}
+                                    @endif
+                                </span>
+                            </div>
+                        </div>
+                        <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-3">
+                                <span class="text"><i class="fa icon-user text"></i> Owner name:</span>
+                            </div>
+                            <div class="col-md-7">
+                                <span class="text">
+                                    @if ($store->owner_name)
+                                        {{ $store->owner_name }}
+                                    @endif
+                                </span>
+                            </div>
+                        </div>
+                        <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-3">
+                                <span class="text"><i class="fa icon-map text"></i> Address:</span>
+                            </div>
+                            <div class="col-md-7">
+                                <span class="text">
+                                    @if ($store->address)
+                                        {{ $store->address }}
+                                    @endif
+                                </span>
+                            </div>
+                        </div>
+                        <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-3">
+                                <span class="text"><i class="fa icon-screen-smartphone text"></i> Phone:</span>
+                            </div>
+                            <div class="col-md-7">
+                                <span class="text">
+                                    @if ($store->phone)
+                                        {{ $store->phone }}
+                                    @endif
+                                </span>
+                            </div>
+                        </div>
+                        <div class="line line-dashed b-b line-lg pull-in"></div>
+
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-3">
+                                <span class="text"><i class="fa icon-envelope-open text"></i> Email:</span>
+                            </div>
+                            <div class="col-md-7">
+                                <span class="text">
+                                    @if ($store->email)
+                                        {{ $store->email }}
+                                    @endif
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
