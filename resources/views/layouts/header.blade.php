2d567b49b29f563fcf07dbbf47e00b1fa5199138
lollipopfly <lollipop.fly@gmail.com>
2016-11-11 14:42:18 +0300

    make favicon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <link rel="stylesheet" href="{!! asset('build/css/style.css') !!}">
+    <link rel="icon" type="image/png" href="{!! asset('favicon.png') !!}" />
 </head>
 <body ng-cloak>
 <div class="app app-header-fixed ">
